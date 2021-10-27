a46a3cd97b66c8a863d40f33e59ed6d1971f416e
liutao <liutaohangzhou@163.com>
2021-10-27 12:23:21 +0800

    Updates

---
+++ assets/index.571fb51d.js
@@ -0,0 +1,1 @@
+import{j as e}from"./vendor.0f4381db.js";const t=()=>e("div",{children:" TestPage "});export{t as default};
---
+++ assets/index.a667ef59.js
@@ -0,0 +1,1 @@
+import{j as e}from"./vendor.0f4381db.js";const d=()=>e("div",{children:" Page404 "});export{d as default};
